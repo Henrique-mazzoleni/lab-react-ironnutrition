6d44b0c33d4f75ee196b95d8a0556eb389abbd54
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-02-24 10:20:52 +0100

    iteration 7 completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
 import AddFoodForm from './components/AddFoodForm';
 import Search from './components/Search';
@@ -7,14 +7,21 @@ import FoodBox from './components/FoodBox';
 import './App.css';
 import foods from './foods.json';
 
+import { Row, Divider, Button } from 'antd';
+
 function App() {
   const [foodList, setFoodList] = useState(foods);
   const [filteredFoodList, setFilteredFoodList] = useState(foods);
   const [search, setSearch] = useState('')
+  const [showForm, setShowForm] = useState(false)
 
   useEffect(()=>{
     setFilteredFoodList(foodList.filter(foodItem => foodItem.name.toLowerCase().includes(search.toLowerCase())))
   },[search, foodList])
+
+  const showFormHandler = () => {
+    setShowForm(currState => !currState)
+  }
 
   const addFoodHandler = (foodObject) => {
     setFoodList(currState => [...currState, foodObject])
@@ -30,10 +37,16 @@ function App() {
 
   return (
     <div className="App">
-      <AddFoodForm onSubmit={addFoodHandler} />
+      {showForm ? 
+        <Fragment>
+          <AddFoodForm onSubmit={addFoodHandler} />
+          <Button onClick={showFormHandler}> Hide Form </Button> 
+        </Fragment>
+        : <Button onClick={showFormHandler}> Add New Food </Button>
+      }
       <Search onSearch={searchHandler} />
-      <h1>Food List</h1>
-      <div className="food-list">
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
         {filteredFoodList.map((foodItem) => (
           <FoodBox
             key={foodItem.name}
@@ -46,7 +59,7 @@ function App() {
             onDelete={deleteItemHandler}
           />
         ))}
-      </div>
+      </Row>
     </div>
   );
 }
